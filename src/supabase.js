16a07658c625488be3ec98000d6fe980909233d0
Immanuel Quansah Eshun <124244985+Qhojoblinks-7@users.noreply.github.com>
2025-01-23 10:07:44 +0000

    create the .env file and updates supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = '/api/rest/v1'; // Use proxy path
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY; // Keep the key secure
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY; // Keep the key secure
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
